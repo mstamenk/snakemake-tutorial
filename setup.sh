0c84d01b2fc8dc294bb66c0e0994aeae1f7d6fc2
Marko Stamenkovic <marko.stamenkovic@epfl.ch>
2017-08-08 11:15:56 +0200

    Corrections

--- setup.sh
+++ setup.sh
@@ -17,16 +17,19 @@ else
 #    source scripts/setup_path.sh # Uncomment if you need other libraries or in order to set an environment with python2
 fi
 # Definition of various path in the architecture of the project
-export CPP=$PWD/cpp
+
+export MYROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export CPP=$MYROOT/cpp
 export CPPSRC=$CPP/src
 
-export TUTO=$PWD
-export SCRIPTS=$PWD/scripts
+export TUTO=$MYROOT
+export SCRIPTS=$MYROOT/scripts
 
-export PYTHON=$PWD/python
+export PYTHON=$MYROOT/python
 
-export RESOURCE=$PWD/resource
-export ROUTINES=$PWD/routines
+export RESOURCE=$MYROOT/resource
+export ROUTINES=$MYROOT/routines
 
 # User-sensitive way to define your project, define the folders that are not the same from one person to the other.
 # Once the repertories updated with a git pull, every user should be able to run the full analysis
@@ -35,7 +38,7 @@ then
     export PLOTS=$HOME/Desktop/plots
 elif [ "$USER" = "COMPLETE HERE" ]
 then
-    export PLOTS=$PWD/plots
+    export PLOTS=$MYROOT/plots
 else
     echo "Don't forget to compelte the paths in the setup.sh in order to run the scripts!"
 fi
